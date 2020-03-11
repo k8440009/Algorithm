b43e60eeb03d26d712e49b827a476daa4cfe2c41
sungslee <k8440009@gmail.com>
2020-03-11 18:02:43 +0900

    add

--- BOJ/16236_1_2_na982.cpp
+++ BOJ/16236_1_2_na982.cpp
@@ -5,10 +5,12 @@
 #include <queue>
 #include <algorithm>
 using namespace std;
+
 struct SHARK
 {
-    int y, x, time;
+	int y, x, time;
 };
+
 const int dy[] = {0, 0, -1, 1};
 const int dx[] = {-1, 1, 0, 0};
 const int MAX = 20;
@@ -21,90 +23,90 @@ SHARK shark;
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-    cin >> N;
-    for (int y = 0; y < N; y++)
-    {
-        for (int x = 0; x < N; x++)
-        {
-            cin >> board[y][x];
-            if (board[y][x] == 9)
-            {
-                shark.y = y, shark.x = x, shark.time = 0;
-                sharkSize = 2, sharkEat = 0;
-                board[y][x] = 0;
-            }
-        }
-    }
+	cin >> N;
+	for (int y = 0; y < N; y++)
+	{
+		for (int x = 0; x < N; x++)
+		{
+			cin >> board[y][x];
+			if (board[y][x] == 9)
+			{
+				shark.y = y, shark.x = x, shark.time = 0;
+				sharkSize = 2, sharkEat = 0;
+				board[y][x] = 0;
+			}
+		}
+	}
 
-    bool isUpdate = true;
-    while (isUpdate)
-    {
-        isUpdate = false;
-        bool visited[MAX][MAX] = {
-            false,
-        };
-        queue<SHARK> q;
-        visited[shark.y][shark.x] = true;
-        q.push(shark);
+	bool isUpdate = true;
+	while (isUpdate)
+	{
+		isUpdate = false;
+		bool visited[MAX][MAX] = {
+			false,
+		};
+		queue<SHARK> q;
+		visited[shark.y][shark.x] = true;
+		q.push(shark);
 
-        // 잡아 먹힐 후보자
-        SHARK candi;
-        candi.y = 20, candi.time = -1;
+		// 잡아 먹힐 후보자
+		SHARK candi;
+		candi.y = 20, candi.time = -1;
 
-        while (!q.empty())
-        {
-            SHARK cur = q.front();
-            q.pop();
-            // 갱신됨, 가장가까운 물고기가 없는 경우
-            if (candi.time != -1 && candi.time < cur.time)
-            {
-                break;
-            }
+		while (!q.empty())
+		{
+			SHARK cur = q.front();
+			q.pop();
+			// 갱신됨, 가장가까운 물고기가 없는 경우
+			if (candi.time != -1 && candi.time < cur.time)
+			{
+				break;
+			}
 
-            if (board[cur.y][cur.x] < sharkSize && board[cur.y][cur.x] != 0)
-            {
-                isUpdate = true;
-                if (candi.y > cur.y || (candi.y == cur.y && candi.x > cur.x))
-                {
-                    candi = cur;
-                }
-            }
+			if (board[cur.y][cur.x] < sharkSize && board[cur.y][cur.x] != 0)
+			{
+				isUpdate = true;
+				if (candi.y > cur.y || (candi.y == cur.y && candi.x > cur.x))
+				{
+					candi = cur;
+				}
+			}
 
-            for (int dir = 0; dir < 4; dir++)
-            {
-                SHARK next;
-                next.y = cur.y + dy[dir];
-                next.x = cur.x + dx[dir];
-                next.time = cur.time + 1;
+			for (int dir = 0; dir < 4; dir++)
+			{
+				SHARK next;
+				next.y = cur.y + dy[dir];
+				next.x = cur.x + dx[dir];
+				next.time = cur.time + 1;
 
-                if (next.y < 0 || next.y >= N || next.x < 0 || next.x >= N)
-                    continue;
+				if (next.y < 0 || next.y >= N || next.x < 0 || next.x >= N)
+					continue;
 
-                if (visited[next.y][next.x] == false && sharkSize >= board[next.y][next.x])
-                {
-                    visited[next.y][next.x] = true;
-                    q.push(next);
-                }
-            }
-        }
+				if (visited[next.y][next.x] == false && sharkSize >= board[next.y][next.x])
+				{
+					visited[next.y][next.x] = true;
+					q.push(next);
+				}
+			}
+		}
 
-        if (isUpdate)
-        {
-            shark = candi;
-            sharkEat += 1;
-            if (sharkEat == sharkSize)
-            {
-                sharkSize += 1;
-                sharkEat = 0;
-            }
-            board[shark.y][shark.x] = 0;
-        }
-    }
+		if (isUpdate)
+		{
+			shark = candi;
+			sharkEat += 1;
+			if (sharkEat == sharkSize)
+			{
+				sharkSize += 1;
+				sharkEat = 0;
+			}
+			board[shark.y][shark.x] = 0;
+		}
+	}
 
-    cout << shark.time << '\n';
-    return 0;
+	cout << shark.time << '\n';
+	return 0;
 }
