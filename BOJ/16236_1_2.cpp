4802eb61c295b02176a4463c81b3a8309f6e5d9d
sungslee <sungslee@student.42seoul.kr>
2020-03-11 18:00:37 +0900

    16236 아기상어

--- BOJ/16236_1_2.cpp
+++ BOJ/16236_1_2.cpp
@@ -1,110 +1,138 @@
-// 아기 상어
+// 아기상어
 // https://www.acmicpc.net/problem/16236
 #include <iostream>
 #include <vector>
 #include <queue>
 #include <algorithm>
 using namespace std;
-struct SHARK
+
+const int MAX = 20 + 1;
+const int INF = 987654321;
+
+const int dr[] = {1, 0, -1, 0};
+const int dc[] = {0, 1, 0, -1};
+
+struct FISH
 {
-    int y, x, time;
+	int row, col, size;
+	int cnt = 0;
+	int time = 0;
 };
-const int dy[] = {0, 0, -1, 1};
-const int dx[] = {-1, 1, 0, 0};
-const int MAX = 20;
-int N;
+
+FISH shark;
+
 int board[MAX][MAX];
 
-int sharkSize, sharkEat;
+int N;
+int g_flag;
 
-SHARK shark;
+void bfs()
+{
+	FISH candi;
+	candi.row = MAX;
+	candi.col = MAX;
+	candi.time = INF;
+	int visited[MAX][MAX];
+	queue<FISH> q;
+
+	fill_n(visited[0], MAX * MAX, 0);
+	q.push(shark);
+	visited[shark.row][shark.col] = 1;
+
+	while (!q.empty())
+	{
+		FISH cur = q.front();
+		q.pop();
+
+		if (board[cur.row][cur.col] != 0 && shark.size > board[cur.row][cur.col] && cur.time <= candi.time)
+		{
+			g_flag = 1;
+			candi.time = cur.time;
+			if (cur.row < candi.row)
+			{
+				candi.row = cur.row;
+				candi.col = cur.col;
+			}
+			else if (cur.row == candi.row && cur.col < candi.col)
+			{
+				candi.row = cur.row;
+				candi.col = cur.col;
+			}
+		}
+
+		if (cur.time > candi.time)
+			break;
+
+		for (int dir = 0; dir < 4; dir++)
+		{
+			FISH next;
+			next.row = cur.row + dr[dir];
+			next.col = cur.col + dc[dir];
+			next.time = cur.time + 1;
+			next.size = board[next.row][next.col];
+
+			if (next.row < 0 || next.row >= N || next.col < 0 || next.col >= N || visited[next.row][next.col])
+				continue;
+			else if (next.size > shark.size)
+				continue;
+			q.push(next);
+			visited[next.row][next.col] = 1;
+		}
+	}
+
+	if (g_flag)
+	{
+		shark.row = candi.row, shark.col = candi.col, shark.time = candi.time;
+		board[shark.row][shark.col] = 0;
+		shark.cnt++;
+		if (shark.cnt == shark.size)
+		{
+			shark.size++;
+			shark.cnt = 0;
+		}
+	}
+}
+
+void solve()
+{
+	while (1)
+	{
+		g_flag = 0;
+		bfs();
+		if (!g_flag)
+			break;
+	}
+}
+
+void print()
+{
+	cout << shark.time << '\n';
+}
+
+void init()
+{
+	cin >> N;
+	for (int r = 0; r < N; r++)
+	{
+		for (int c = 0; c < N; c++)
+		{
+			cin >> board[r][c];
+			if (board[r][c] == 9)
+			{
+				shark.row = r, shark.col = c, shark.size = 2;
+				board[r][c] = 0;
+			}
+		}
+	}
+}
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-    cin >> N;
-    for (int y = 0; y < N; y++)
-    {
-        for (int x = 0; x < N; x++)
-        {
-            cin >> board[y][x];
-            if (board[y][x] == 9)
-            {
-                shark.y = y, shark.x = x, shark.time = 0;
-                sharkSize = 2, sharkEat = 0;
-                board[y][x] = 0;
-            }
-        }
-    }
-
-    bool isUpdate = true;
-    while (isUpdate)
-    {
-        isUpdate = false;
-        bool visited[MAX][MAX] = {
-            false,
-        };
-        queue<SHARK> q;
-        visited[shark.y][shark.x] = true;
-        q.push(shark);
-
-        // 잡아 먹힐 후보자
-        SHARK candi;
-        candi.y = 20, candi.time = -1;
-
-        while (!q.empty())
-        {
-            SHARK cur = q.front();
-            q.pop();
-            // 갱신됨, 가장가까운 물고기가 없는 경우
-            if (candi.time != -1 && candi.time < cur.time)
-            {
-                break;
-            }
-
-            if (board[cur.y][cur.x] < sharkSize && board[cur.y][cur.x] != 0)
-            {
-                isUpdate = true;
-                if (candi.y > cur.y || (candi.y == cur.y && candi.x > cur.x))
-                {
-                    candi = cur;
-                }
-            }
-
-            for (int dir = 0; dir < 4; dir++)
-            {
-                SHARK next;
-                next.y = cur.y + dy[dir];
-                next.x = cur.x + dx[dir];
-                next.time = cur.time + 1;
-
-                if (next.y < 0 || next.y >= N || next.x < 0 || next.x >= N)
-                    continue;
-
-                if (visited[next.y][next.x] == false && sharkSize >= board[next.y][next.x])
-                {
-                    visited[next.y][next.x] = true;
-                    q.push(next);
-                }
-            }
-        }
-
-        if (isUpdate)
-        {
-            shark = candi;
-            sharkEat += 1;
-            if (sharkEat == sharkSize)
-            {
-                sharkSize += 1;
-                sharkEat = 0;
-            }
-            board[shark.y][shark.x] = 0;
-        }
-    }
-
-    cout << shark.time << '\n';
-    return 0;
+	init();
+	solve();
+	print();
 }
