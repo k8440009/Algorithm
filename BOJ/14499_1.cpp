d5aaa50af334e37e34e972bc69ba115ba0151df9
sungslee <k8440009@gmail.com>
2020-03-11 19:16:58 +0900

    14499 주사위 굴리기 c++

--- BOJ/14499_1.cpp
+++ BOJ/14499_1.cpp
@@ -1,105 +1,123 @@
 // 주사위 굴리기
 // https://www.acmicpc.net/problem/14499
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 using namespace std;
-const int MAX = 20;
-int N, M, K;
-int dy[4] = {0, 0, -1, 1};
-int dx[4] = {1, -1, 0, 0};
+
+const int MAX = 20 + 1;
+const int dr[] = {0, 0, -1, 1};
+const int dc[] = {1, -1, 0, 0};
+
+int N, M, Y, X, K;
 int board[MAX][MAX];
 int dice[6];
-void moveDice(int dir)
+vector<int> comd;
+
+void print()
 {
-    int ndice[6];
-    switch (dir)
-    {
-    case 0:
-        ndice[0] = dice[0];
-        ndice[5] = dice[1];
-        ndice[2] = dice[2];
-        ndice[4] = dice[3];
-        ndice[1] = dice[4];
-        ndice[3] = dice[5];
-        break;
-    case 1:
-        ndice[0] = dice[0];
-        ndice[4] = dice[1];
-        ndice[2] = dice[2];
-        ndice[5] = dice[3];
-        ndice[3] = dice[4];
-        ndice[1] = dice[5];
-        break;
-    case 2:
-        ndice[3] = dice[0];
-        ndice[0] = dice[1];
-        ndice[1] = dice[2];
-        ndice[2] = dice[3];
-        ndice[4] = dice[4];
-        ndice[5] = dice[5];
-        break;
-    case 3:
-        ndice[1] = dice[0];
-        ndice[2] = dice[1];
-        ndice[3] = dice[2];
-        ndice[0] = dice[3];
-        ndice[4] = dice[4];
-        ndice[5] = dice[5];
-        break;
-    default:
-        break;
-    }
-    for (int i = 0; i < 6; i++)
-    {
-        dice[i] = ndice[i];
-    }
+	cout << dice[0] << '\n';
 }
+
+void changeDice(int dir)
+{
+	// 동
+	if (dir == 0)
+	{
+		int temp;
+
+		temp = dice[2];
+
+		dice[2] = dice[0];
+		dice[0] = dice[3];
+		dice[3] = dice[5];
+		dice[5] = temp;
+	}
+	// 서
+	else if (dir == 1)
+	{
+		int temp;
+
+		temp = dice[3];
+
+		dice[3] = dice[0];
+		dice[0] = dice[2];
+		dice[2] = dice[5];
+		dice[5] = temp;
+	}
+	// 북
+	else if (dir == 2)
+	{
+		int temp;
+
+		temp = dice[1];
+
+		dice[1] = dice[0];
+		dice[0] = dice[4];
+		dice[4] = dice[5];
+		dice[5] = temp;
+	}
+	// 남
+	else
+	{
+		int temp;
+
+		temp = dice[5];
+
+		dice[5] = dice[4];
+		dice[4] = dice[0];
+		dice[0] = dice[1];
+		dice[1] = temp;
+	}
+}
+void solve()
+{
+	for (int i = 0; i < K; i++)
+	{
+		int dir = comd[i];
+		int nr = Y + dr[dir];
+		int nc = X + dc[dir];
+
+		if (nr < 0 || nr >= N || nc < 0 || nc >= M)
+			continue;
+		changeDice(dir);
+		if (board[nr][nc] == 0)
+			board[nr][nc] = dice[5];
+		else
+		{
+			dice[5] = board[nr][nc];
+			board[nr][nc] = 0;
+		}
+		print();
+		Y = nr;
+		X = nc;
+	}
+}
+
+void init()
+{
+	cin >> N >> M >> Y >> X >> K;
+	for (int r = 0; r < N; r++)
+	{
+		for (int c = 0; c < M; c++)
+		{
+			cin >> board[r][c];
+		}
+	}
+	comd.resize(K);
+	for (int i = 0; i < K; i++)
+	{
+		cin >> comd[i];
+		comd[i]--;
+	}
+}
+
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-    int x, y;
-    cin >> N >> M >> y >> x >> K;
-    for (int y = 0; y < N; y++)
-    {
-        for (int x = 0; x < M; x++)
-        {
-            cin >> board[y][x];
-        }
-    }
-
-    int dir = 0;
-    for (int i = 0; i < K; i++)
-    {
-        cin >> dir;
-        dir--;
-
-        int ny = y + dy[dir];
-        int nx = x + dx[dir];
-
-        if (ny < 0 || ny >= N || nx < 0 || nx >= M)
-            continue;
-
-        moveDice(dir);
-
-        if (board[ny][nx] == 0)
-        {
-            board[ny][nx] = dice[3];
-        }
-        else
-        {
-            dice[3] = board[ny][nx];
-            board[ny][nx] = 0;
-        }
-
-        y = ny;
-        x = nx;
-
-        cout << dice[1] << '\n';
-    }
-
-    return 0;
+	init();
+	solve();
 }
