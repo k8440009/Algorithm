f0123e328874c40d011d0ae8d965f1fe26511af8
sungslee <k8440009@gmail.com>
2020-03-29 02:54:53 +0900

    2018 kakao blind test 뉴스 트래픽

---
+++ programmers/17677_1_1.py
@@ -0,0 +1,84 @@
+# 뉴스 클러스터링
+# https://programmers.co.kr/learn/courses/30/lessons/17677
+
+import math
+def union(a, b):
+	l = []
+
+	tmp = ""
+	for i in range(len(a)):
+		data = a[i]
+		if tmp != data : 
+			tmp = data
+			cnt = max(a.count(data), b.count(data))
+			while cnt :
+				l.append(data)
+				cnt -=1
+		else : 
+			continue
+	tmp = ""
+	for i in range(len(b)):
+		data = b[i]
+		if tmp != data :
+			if l.count(data) == 0:
+				tmp = data
+				cnt = b.count(data)
+				while cnt :
+					l.append(data)
+					cnt -=1
+		else : 
+			continue
+	return len(l)
+
+
+def inter(a, b):
+	l = []
+
+	tmp = ""
+	for i in range(len(a)):
+		data = a[i]
+		if tmp != data :
+			if a.count(data) > 0 and b.count(data) > 0:
+				tmp = data
+				cnt = min(a.count(data), b.count(data))
+				while cnt :
+					l.append(data)
+					cnt -=1
+		else : 
+			continue
+	return len(l)
+
+def makeList(str) :
+	l = []
+	for i in range(len(str) - 1) :
+		tmp = str[i] + str[i + 1]
+		if str[i] >= 'a' and str[i] <= 'z' and str[i + 1] >= 'a' and str[i + 1] <= 'z' : 
+			l.append(tmp)
+	return l
+
+def solution(str1, str2):
+	answer = 0
+	a = []
+	b = []
+
+	# 다중집합 구현	
+	a = makeList(str1.lower())
+	b = makeList(str2.lower())
+
+	a.sort()
+	b.sort()
+	
+	if len(a) == 0 and len(b) == 0:
+		answer = 1 * 65536
+	else :
+		sun = inter(a,b)
+		mother = union(a,b)
+		answer = (sun / mother) * 65536
+		answer = math.trunc(answer)
+	print(answer)
+	return answer
+
+#solution("FRANCE", "FRENCH")
+#solution("aaaabb", "bbbbaa")
+#solution("handshake", "shake hands")
+#solution("FRENCH", "")
---
+++ programmers/17677_1_2.py
@@ -0,0 +1,40 @@
+# 뉴스 클러스터링
+# https://programmers.co.kr/learn/courses/30/lessons/17677
+import re
+from collections import Counter
+
+
+def solution(str1, str2):
+    answer = 0
+
+    list1 = []
+    list2 = []
+
+    p = re.compile('[a-zA-Z]+')
+
+    for i in range(len(str1)-1):
+        m1 = p.match(str1[i].upper())
+        m2 = p.match(str1[i+1].upper())
+
+        if m1 and m2:
+            list1.append(str(m1.group()+m2.group()))
+
+    for i in range(len(str2)-1):
+        m1 = p.match(str2[i].upper())
+        m2 = p.match(str2[i+1].upper())
+
+        if m1 and m2:
+            list2.append(str(m1.group()+m2.group()))
+
+    m1 = Counter(list1)
+    m2 = Counter(list2)
+
+    inter = list((m1&m2).elements())
+    union = list((m1|m2).elements())
+
+    if len(union) == 0:
+        answer = 65536
+    else:
+        answer = int(len(inter) / len(union) * 65536)
+
+    return answer
