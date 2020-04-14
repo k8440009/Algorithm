2fa2279c104be64719b3a8bf2cafbc2370f3291a
sungslee <k8440009@gmail.com>
2020-04-14 19:32:25 +0900

    2115. [모의 SW 역량테스트] 벌꿀채취 수정

--- SWEA/2115_1_1.cpp
+++ SWEA/2115_1_1.cpp
@@ -11,39 +11,39 @@ int visited[12][12];
 int selected[7];
 vector<pair<int, int>> a, b;
 
-void dfs(int cnt, int capacity, int usr)
+void dfs(int cnt, int prev, int capacity, int usr)
 {
+	if (capacity > C)
+		return;
 	if (cnt == M)
 	{
-		if (capacity <= C)
+		if (usr == 0)
 		{
-			if (usr == 0)
+			int total = 0;
+			for (int i = 0; i < M; i++)
 			{
-				int total = 0;
-				for (int i = 0; i < M; i++)
+				if (selected[i])
 				{
-					if (selected[i])
-					{
-						int data = board[a[i].first][a[i].second];
-						total += (data * data);
-					}
+					int data = board[a[i].first][a[i].second];
+					total += (data * data);
 				}
-				sum_a = max(sum_a, total);
 			}
-			else
-			{
-				int total = 0;
-				for (int i = 0; i < M; i++)
-				{
-					if (selected[i])
-					{
-						int data = board[b[i].first][b[i].second];
-						total += (data * data);
-					}
-				}
-				sum_b = max(sum_b, total);
-			}
+			sum_a = max(sum_a, total);
 		}
+		else
+		{
+			int total = 0;
+			for (int i = 0; i < M; i++)
+			{
+				if (selected[i])
+				{
+					int data = board[b[i].first][b[i].second];
+					total += (data * data);
+				}
+			}
+			sum_b = max(sum_b, total);
+		}
+
 		return;
 	}
 
@@ -51,21 +51,24 @@ void dfs(int cnt, int capacity, int usr)
 	{
 		if (selected[i])
 			continue;
+		if (prev > i)
+			continue;
+
 		selected[i] = 1;
 		if (usr == 0)
-			dfs(cnt + 1, capacity + board[a[i].first][a[i].second], usr);
+			dfs(cnt + 1, i, capacity + board[a[i].first][a[i].second], usr);
 		else
-			dfs(cnt + 1, capacity + board[b[i].first][b[i].second], usr);
+			dfs(cnt + 1, i, capacity + board[b[i].first][b[i].second], usr);
 		selected[i] = 0;
 	}
-	dfs(cnt + 1, capacity, usr);
+	dfs(cnt + 1, prev, capacity, usr);
 }
 
 void solve()
 {
 	sum_a = 0, sum_b = 0;
-	dfs(0, 0, 0);
-	dfs(0, 0, 1);
+	dfs(0, 0, 0, 0);
+	dfs(0, 0, 0, 1);
 	answer = max(answer, sum_a + sum_b);
 }
 
