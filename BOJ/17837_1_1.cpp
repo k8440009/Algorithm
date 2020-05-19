6c9a3e59387a680cabffb1fdc04988f4b9a958da
sungslee <k8440009@gmail.com>
2020-05-19 22:45:11 +0900

    삼성 기출 새로운 게임 2

--- BOJ/17837_1_1.cpp
+++ BOJ/17837_1_1.cpp
@@ -1,18 +1,21 @@
+// 새로운 게임 2 - 1회 6시간
+// https://www.acmicpc.net/problem/17837
 #include <iostream>
 #include <vector>
 using namespace std;
 
-struct INFO
-{
-    int r,c,dir;
-};
+const int INF = 987654321;
+const int dr[] = {0,0,0,-1,1};
+const int dc[] = {0,1,-1,0,0};
 
-const int dr[] = {0,0,-1,1};
-const int dc[] = {1,-1,0,0}; 
+struct HORSE
+{
+    int r, c, dir;
+};
 int N,K, answer;
-int state_board[14][14];
-vector <INFO> board[14][14];
-vector <INFO> knights;
+int color[14][14];
+vector <int> board[14][14];
+vector <HORSE> loc;
 
 int change_dir(int dir)
 {
@@ -20,52 +23,133 @@ int change_dir(int dir)
         return 2;
     else if(dir == 2)
         return 1;
-    else if (dir == 3)
-        return 2;
+    else if(dir == 3)
+        return 4;
     else if(dir == 4)
         return 3;
 }
 
-void blue(INFO cur, int size)
+int red(vector<int> data, int k)
 {
-    cur.dir = change_dir(cur.dir);
-    int nr = cur.r + dr[cur.dir], nc = cur.c + dc[cur.dir];
-    if(nr < 0 || nr >= N || nc < 0 || nc >= N || state_board[nr][nc] == 2)
-    board[nr][nc].push_back(cur);
+    int cnt = 0;
+    int nr = loc[k].r + dr[loc[k].dir], nc = loc[k].c + dc[loc[k].dir];
+    for(int i = data.size() - 1; i >= 0; i--)
+    {
+        board[nr][nc].push_back(data[i]);
+        loc[data[i]].r = nr, loc[data[i]].c = nc;
+    }
+    cnt = board[nr][nc].size();
+    return cnt;
+}
+int white(vector<int> data, int k)
+{
+    int cnt = 0;
+    int nr = loc[k].r + dr[loc[k].dir], nc = loc[k].c + dc[loc[k].dir];
+    for(int i = 0; i < data.size(); i++)
+    {
+        board[nr][nc].push_back(data[i]);
+        loc[data[i]].r = nr, loc[data[i]].c = nc;
+    }
+    cnt = board[nr][nc].size();
+    return cnt;
+}
+
+int blue(int k)
+{
+    int cnt = 0;
+    int nr = loc[k].r + dr[loc[k].dir], nc = loc[k].c + dc[loc[k].dir];
+    if(nr < 1 || nr >= N + 1 || nc < 1 || nc >= N + 1 || color[nr][nc] == 2)
+        return cnt;
+    else
+    {
+        int idx;
+        vector <int> tmp;
+        for(int i = 0; i < board[loc[k].r][loc[k].c].size(); i++)
+        {
+            if(k == board[loc[k].r][loc[k].c][i])
+            {
+                idx = i;
+                break;
+            }
+        }
+        for(int i = idx; i < board[loc[k].r][loc[k].c].size(); i++)
+            tmp.push_back(board[loc[k].r][loc[k].c][i]);
+        for(int i = 0; i < tmp.size(); i++)
+            board[loc[k].r][loc[k].c].pop_back();
+        if(color[nr][nc] == 0)
+            cnt = white(tmp,k);
+        else if(color[nr][nc] == 1)
+            cnt = red(tmp,k);
+    }
+    return cnt;
+    
 }
 void solve()
 {
-    
-    int time = 0;
-    while (time < 1000)
+    int time = 1;
+    while (time <= 1000)
     {
         for(int k = 0; k < K; k++)
         {
-            INFO cur = knights[k];
-            int nr = cur.r + dr[cur.dir], nc = cur.c + dc[cur.dir];
-            
+            int cnt = 0;
+            int nr = loc[k].r + dr[loc[k].dir], nc = loc[k].c + dc[loc[k].dir];
+            if(nr < 1 || nr >= N + 1 || nc < 1 || nc >= N + 1 || color[nr][nc] == 2)
+            {
+                loc[k].dir = change_dir(loc[k].dir);
+                cnt = blue(k);
+            }
+            else
+            {
+                int idx;
+                vector <int> tmp;
+                for(int i = 0; i < board[loc[k].r][loc[k].c].size(); i++)
+                {
+                    if(k == board[loc[k].r][loc[k].c][i])
+                    {
+                        idx = i;
+                        break;
+                    }
+                }
+                for(int i = idx; i < board[loc[k].r][loc[k].c].size(); i++)
+                    tmp.push_back(board[loc[k].r][loc[k].c][i]);
+                for(int i = 0; i < tmp.size(); i++)
+                    board[loc[k].r][loc[k].c].pop_back();
+                if(color[nr][nc] == 0)
+                    cnt = white(tmp,k);
+                else if(color[nr][nc] == 1)
+                    cnt = red(tmp,k);
+            }
+
+            if(cnt >= 4)
+            {
+                answer = time;
+                return ;
+            }
         }
         time++;
     }
-    
+       
 }
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    answer = -1;
+
+    answer = INF;
     cin >> N >> K;
-    for(int r = 0; r < N; r++)
-        for(int c = 0; c < N; c++)
-            cin >> state_board[r][c];
-    for(int k = 0; k < K; k++)
+    for(int r = 1; r <= N; r++)
+        for(int c = 1; c <= N; c++)
+            cin >> color[r][c];
+    for(int i = 0; i < K; i++)
     {
-        INFO tmp;
+        HORSE tmp;
         cin >> tmp.r >> tmp.c >> tmp.dir;
-        tmp.r -=1, tmp.c -=1, tmp.dir -=1;
-        board[tmp.r][tmp.c] = tmp;
-        knights.push_back(tmp);
+        board[tmp.r][tmp.c].push_back(i);
+        loc.push_back(tmp);
     }
     solve();
+    if(answer == INF)
+        answer = -1;
+    cout << answer << '\n';
 }
