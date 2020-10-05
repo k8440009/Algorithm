e8b765dbf046324d6f3a6ffd74f5fcddeac2dbca
sungslee <k8440009@gmail.com>
2020-10-05 22:34:06 +0900

    samsung coding test 낚시왕

--- BOJ/17143_3.cpp
+++ BOJ/17143_3.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 using namespace std;
 
-const int MAX = 100 + 1;
+const int MAX = 100 + 2;
 const int dr[] = {0,-1,1,0,0};
 const int dc[] = {0,0,0,1,-1};
 
@@ -17,10 +17,114 @@ struct SHARK
 vector <SHARK> board[MAX][MAX];
 int R, C, M;
 
+void move()
+{
+	vector <SHARK> next[MAX][MAX];
+
+	for (int r = 1; r <= R; r++)
+	{
+		for (int c = 1; c <= C; c++)
+		{
+			if (!board[r][c].empty())
+			{
+				SHARK cur = board[r][c][0];
+				if (cur.dir == 1 || cur.dir == 2)
+				{
+					int nr, nc;
+					int balance = cur.speed % (2 * (R - 1));
+					int cnt = balance;
+					while (cnt > 0)
+					{
+						nr = cur.r + dr[cur.dir], nc = cur.c + dc[cur.dir];
+						if (nr < 1 || nr > R || nc < 1 || nc > C)
+						{
+							if (cur.dir == 1)
+								cur.dir = 2;
+							else
+								cur.dir = 1;
+							continue;
+						}
+						cur.r = nr, cur.c = nc;
+						cnt--;
+					}
+					if (!next[cur.r][cur.c].empty())
+					{
+						if (cur.size > next[cur.r][cur.c][0].size)
+						{
+							next[cur.r][cur.c].pop_back();
+							next[cur.r][cur.c].push_back(cur);
+						}
+					}
+					else
+						next[cur.r][cur.c].push_back(cur);
+				
+				}
+				else
+				{
+					int balance = cur.speed % (2 * (C - 1));
+					int cnt = balance;
+					int nr, nc;
+					while (cnt > 0)
+					{
+						nr = cur.r + dr[cur.dir], nc = cur.c + dc[cur.dir];
+						if (nr < 1 || nr > R || nc < 1 || nc > C)
+						{
+							if (cur.dir == 3)
+								cur.dir = 4;
+							else
+								cur.dir = 3;
+							continue;
+						}
+						cur.r = nr, cur.c = nc;
+						cnt--;
+					}
+					if (!next[cur.r][cur.c].empty())
+					{
+						if (cur.size > next[cur.r][cur.c][0].size)
+						{
+							next[cur.r][cur.c].pop_back();
+							next[cur.r][cur.c].push_back(cur);
+						}
+					}
+					else
+						next[cur.r][cur.c].push_back(cur);
+				}
+			}
+		}
+	}	
+	for (int r = 1; r <= R; r++)
+	{
+		for (int c = 1; c <= C; c++)
+		{
+			board[r][c].clear();
+			if (!next[r][c].empty())
+				board[r][c] = next[r][c];
+		}
+	}
+}
+
+void print_board()
+{
+	for (int r = 1; r <= R; r++)
+	{
+		for (int c = 1; c <= C; c++)
+		{
+			if (!board[r][c].empty())
+				cout << board[r][c][0].size << ' ';
+			else
+				cout << 0 << ' ';
+		}
+		cout << '\n';
+	}
+}
 int	fishing(int c)
 {
 	int r = 1, size = 0;
-
+	/*
+	cout << '\n';
+	cout << "loc : " << c << '\n';
+	print_board();
+	*/
 	while (r <= R)
 	{
 		if (!board[r][c].empty())
@@ -29,6 +133,7 @@ int	fishing(int c)
 			board[r][c].pop_back();
 			break;
 		}
+		r++;
 	}
 	return (size);
 }
@@ -48,11 +153,11 @@ int main()
 
 	int i = 0;
 	int answer = 0;
-	while (i <= R)
+	while (i <= C)
 	{
 		i++;
 		answer += fishing(i);
-		// move();
+		move();
 	}
 	cout << answer << '\n';
 }
