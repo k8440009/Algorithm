8d17c5bd65ce2e4d09bf9b971295720b1ad2dba7
sungslee <k8440009@gmail.com>
2021-12-13 23:29:47 +0900

    java start

---
+++ BOJ/Java/1912.java
@@ -0,0 +1,34 @@
+package Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+class Main{
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++){
+            numbers.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int max = numbers.get(0);
+        int sum = 0;
+        for (int i = 0; i < n; i++){
+            sum += numbers.get(i);
+            max = Math.max(max, sum);
+            if (sum < 0)
+                sum = 0;
+        }
+        System.out.print(max);
+	}
+}
