a0426655c8c7cf2974a23b624e0a1ed7eee78d2f
sungslee <k8440009@gmail.com>
2022-07-15 01:46:49 +0900

    finsi 72410

--- Java/Programers/Java_92334.java
+++ Java/Programers/Java_92334.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Java_92334 {
     public static void main(String[] args) {
         Solution test = new Solution();
@@ -6,8 +8,23 @@ public class Java_92334 {
 }
 
 class Solution {
-    public String solution(String new_id) {
-        String answer = "";
-        return answer;
+    public int[] solution(String[] id_list, String[] reports, int k) {
+        int [] data = new int[4];
+        int idCnt = id_list.length;
+
+        for (String id : id_list) {
+
+        }
+
+        // ArrayList <String> arr[idCnt] = new ArrayList<String>();
+
+        for (String report : reports) {
+            String [] tokens = report.split(" ");
+            String id = tokens[0];
+            String problemId = tokens[1];
+
+        }
+
+       return data;
     }
 }
