f60a251f18dd3d37bc4acf32a52532d28387f70a
sungslee <k8440009@gmail.com>
2022-07-23 22:59:15 +0900

    42889

---
+++ Java/Programmers/Java_42889.java
@@ -0,0 +1,15 @@
+public class Java_42889 {
+    public static void main(String[] args) {
+        Solution_42889 test = new Solution_42889();
+        int N = 5;
+        int [] stages = {2, 1, 2, 6, 2, 4, 3, 3};
+        test.solution(N, stages);
+    }
+}
+
+class Solution_42889 {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = {};
+        return answer;
+    }
+}
