d11136fd5cd07f599171eae4ca58f25c78ae4aca
sungslee <k8440009@gmail.com>
2022-08-03 02:50:04 +0900

    finish 92342

--- Java/Programmers/Java_92342.java
+++ Java/Programmers/Java_92342.java
@@ -2,45 +2,70 @@ import java.util.ArrayList;
 
 public class Java_92342 {
     public static void main(String[] args) {
-        
+        Solution_92342 test = new Solution_92342();
+
+        // int n = 5;
+        // int [] info = {2,1,1,1,0,0,0,0,0,0,0};
+        // int n = 1;
+        // int [] info = {1,0,0,0,0,0,0,0,0,0,0};
+        // int n = 9;
+        // int [] info = {0,0,1,2,0,1,1,1,1,1,1};
+        int n = 10;
+        int [] info = {0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 3};
+
+        int [] results = test.solution(n, info);
+
+        System.out.println();
+        for (int result : results) {
+            System.out.print(result + " ");
+        }
+
     }
 }
 
-class Solution {
+class Solution_92342 {
     static int N;
     static int maxPoint = 0;
-    static int apeachPoint = 0;
+    static int maxLength = 11;
     ArrayList <Integer> data = new ArrayList<>();
-    int [] points = new int [10];
+    int [] points = new int [11];
+
     public int[] solution(int n, int[] info) {
-        int[] answer = {};
+        int[] answer = null;
 
         N = n;
 
-        apeachPoint = getPoint(info);
-        int [] result = new int [10];
-        for (int i = 0; i < 10; i++) {
-            result[i] += 1;
-            dupCombination(i, 1, result, info);
-            result[i] -= 1;
+        int [] result = new int [11];
+        dupCombination(0, 0, result, info);
+        
+        if (maxPoint == 0) {
+            answer = new int[1];
+            answer[0] = -1;
+        } else {
+            answer = new int[11];
+            for (int i = 0; i < 11; i++) {
+                answer[i] = points[i];
+            }
         }
         return answer;
     }
-
+    
     private void dupCombination (int index, int cnt, int [] result, int [] info) {
         if (cnt == N) {
-            int lianPoint = getPoint(result);
-            
-            if (lianPoint > apeachPoint) {
-                int diff = lianPoint - apeachPoint;
-                // getPoint(info, result);
-                if (maxPoint >= diff) {
-                    maxPoint = diff;
-    
-                    if (maxPoint == diff) {
-                        int chk = chkRowPoint(points, result);
-                    } else {
-                        for (int i = 0; i < 10; i++) {
+            int point = getDiffPoint(result, info);
+
+            if (point != -1) {
+                if (point >= maxPoint) {
+                    
+                    int chk = 1;
+                    if (maxPoint == point) {
+                        chk = chklowPoint(points, result);
+                    }
+                    
+                    maxPoint = point;
+
+                    if (chk == 1) {
+                        for (int i = 0; i < 11; i++) {
                             points[i] = result[i];
                         }
                     }
@@ -49,18 +74,47 @@ class Solution {
             return ;
         }
 
-        
-        for (int i = index; i < 10; i++) {
+        for (int i = index; i < 11; i++) {
             result[i] += 1;
-            dupCombination(index, cnt + 1, result, info);
+            dupCombination(i, cnt + 1, result, info);
             result[i] -= 1;
         }
     }
 
-    private int chkRowPoint(int [] before, int [] after) {
+    private int getDiffPoint(int [] lian, int [] apeach) {
+        int res = -1;
+        int lianPoint = 0;
+        int apeachPoint = 0;
+
+        for (int i = 0; i < 10; i++) {
+            int point = 10 - i;
+
+            if (lian[i] == 0 && apeach[i] == 0) {
+                continue;
+            }else if (lian[i] == 0 && apeach[i] != 0) {
+                apeachPoint += point;
+            } else if (lian[i] != 0 && apeach[i] == 0) {
+                lianPoint += point;
+            } else {
+                if (lian[i] > apeach[i]) {
+                    lianPoint += point;
+                } else {
+                    apeachPoint += point;
+                }
+            }
+        }
+
+        if (lianPoint > apeachPoint) {
+            res = lianPoint - apeachPoint;
+        }
+
+        return res;
+
+    }
+    private int chklowPoint(int [] before, int [] after) {
         int res = 0;
 
-        for (int i = 9; i >=0; i--) {
+        for (int i = 10; i >=0; i--) {
             if (before[i] == 0 && after[i] == 0) {
                 continue;
             }
@@ -77,15 +131,10 @@ class Solution {
                     break;
                 } else if (before[i] < after[i]) {
                     res = 1;
+                    break;
                 }
             }
         }
-
-        return res;
-    }
-
-    private int getPoint(int [] points) {
-        int res = -1;
         return res;
     }
 }
