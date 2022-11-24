54e9059a68118ea3db8da7a27f32c41d9d693781
sungslee <k8440009@gmail.com>
2022-11-25 00:44:32 +0900

    344 투 포인터

--- leetcode/344_Reverse String_0.py
+++ leetcode/344_Reverse String_0.py
@@ -1,3 +1,9 @@
+"""
+    문자열 뒤집기
+    - 리스트 reverse 풀아
+
+    https://leetcode.com/problems/reverse-string/
+"""
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         """
---
+++ leetcode/344_Reverse String_1.py
@@ -0,0 +1,13 @@
+"""
+    문자열 뒤집기
+    - 투 포인터
+
+    https://leetcode.com/problems/reverse-string/
+"""
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
