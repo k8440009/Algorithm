69dd68b8e21debe5c451495f252ffd3c32e11048
sungslee <k8440009@gmail.com>
2023-01-02 04:04:57 +0900

    add 207

---
+++ leetcode/207. Course Schedule_1.py
@@ -0,0 +1,39 @@
+"""
+    코스 스케줄
+    0을 완료하기 위해서는 1을 끝내야 한다는 것을 [0, 1] 쌍으로 표현하는 n개의 코스가 있다.
+    코스 개수 n과 이 쌍들을 입력으로 받았을 때 모든 코스가 완료 가능한지 판별하라
+
+    dfs로 순환 구조 판별 시간 초과
+"""
+import collections
+from typing import List
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = collections.defaultdict(list)
+        # 그래프 구성
+        for x, y in prerequisites:
+            graph[x].append(y)
+
+        traced = set()
+        def dfs(i):
+            # 순환 구조이면 False
+            if i in traced:
+                return False
+
+            traced.add(i)
+            for y in graph[i]:
+                if not dfs(y):
+                    return False
+            # 탐색 종료 후 순환 노드 삭제
+            traced.remove(i)
+
+            return True
+
+        # 순환 구조 판별
+        for x in list(graph):
+            if not dfs(x):
+                return False
+
+        return True
---
+++ leetcode/207. Course Schedule_2.py
@@ -0,0 +1,45 @@
+"""
+    코스 스케줄
+    0을 완료하기 위해서는 1을 끝내야 한다는 것을 [0, 1] 쌍으로 표현하는 n개의 코스가 있다.
+    코스 개수 n과 이 쌍들을 입력으로 받았을 때 모든 코스가 완료 가능한지 판별하라
+
+    dfs로 순환 구조 판별
+    가지치기를 이용한 최적화 : 한 번 망분했던 그래프는 두 번 이상 방문하지 않도록 수정
+"""
+import collections
+from typing import List
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = collections.defaultdict(list)
+        # 그래프 구성
+        for x, y in prerequisites:
+            graph[x].append(y)
+
+        traced = set()
+        visited = set()
+        def dfs(i):
+            # 순환 구조이면 False
+            if i in traced:
+                return False
+            # 이미 방문했던 노드이면 True
+            if i in visited:
+                return True
+
+            traced.add(i)
+            for y in graph[i]:
+                if not dfs(y):
+                    return False
+            # 탐색 종료 후 순환 노드 삭제
+            traced.remove(i)
+            visited.add(i)
+
+            return True
+
+        # 순환 구조 판별
+        for x in list(graph):
+            if not dfs(x):
+                return False
+
+        return True
