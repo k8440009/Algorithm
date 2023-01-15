b8254d2dc5a4d580bafa391e2d69eb0269dc7922
sungslee <k8440009@gmail.com>
2023-01-16 01:32:42 +0900

    add 310

---
+++ leetcode/310. Minimum Height Trees_1.py
@@ -0,0 +1,42 @@
+"""
+    최소 높이 트리
+    - 단계별 리프 노드 제거
+
+    1. 양방향 그래프 구성
+    2. 길이가 1인 리프 노드 추가
+    3. 루트 노드만 남을 때 까지 반복 제거
+"""
+import collections
+from typing import List
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        if n <= 1:
+            return [0]
+
+        # 양방향 그래프 구성
+        graph = collections.defaultdict(list)
+
+        for i, j in edges:
+            graph[i].append(j)
+            graph[j].append(i)
+
+        # 첫번째 리프 노드 추가
+        leaves = []
+        for i in range(n + 1):
+            if len(graph[i]) == 1:
+                leaves.append(i)
+
+        # 루트 노드만 남을 때까지 반복 제거
+        while n > 2:
+            n -= len(leaves)
+            new_leaves = []
+            for leaf in leaves:
+                neighbor = graph[leaf].pop()
+                graph[neighbor].remove(leaf)
+
+                if len(graph[neighbor]) == 1:
+                    new_leaves.append(neighbor)
+
+            leaves = new_leaves
+
+        return leaves
