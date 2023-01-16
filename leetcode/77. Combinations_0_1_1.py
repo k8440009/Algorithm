b701669c26854b0615f9aa43428a0e03add65812
sungslee <k8440009@gmail.com>
2023-01-17 02:13:07 +0900

    review 77

---
+++ leetcode/77. Combinations_0_1_1.py
@@ -0,0 +1,29 @@
+"""
+    전체 수 n을 입력 받아 k의 조합을 리턴하여라
+
+    n = 4, k = 2
+
+    dfs
+"""
+from typing import List
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        result = []
+
+        def dfs(elements, start: int, k: int):
+            if k == 0:
+                result.append(elements[:])
+                return
+
+            # 자신 이전의 모든 값을 고정하여 재귀 호출
+            for i in range(start, n + 1):
+                elements.append(i)
+                dfs(elements, i + 1, k - 1)
+                elements.pop()
+
+        dfs([], 1, k)
+        return result
+
+a = Solution()
+n, k = 4, 2
+print(a.combine(n, k))
