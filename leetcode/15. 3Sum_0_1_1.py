e61b5b55226ca2e1951a721bc073ba6dfcaa13a9
sungslee <k8440009@gmail.com>
2023-01-20 04:58:06 +0900

    reivew 15

--- leetcode/15. 3Sum_0_1_1.py
+++ leetcode/15. 3Sum_0_1_1.py
@@ -6,25 +6,34 @@
     time out
 https://leetcode.com/problems/3sum/
 """
+
 from typing import List
-
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        result = []
+        results = []
         nums.sort()
 
-        # 브루트 포스 n ^ 3
+        # n ^ 3
         for i in range(len(nums) - 2):
             # 중복 pass
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
+
             for j in range(i + 1, len(nums) - 1):
+                # 중복 pass
                 if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
+
                 for k in range(j + 1, len(nums)):
+                    # 중복 pass
                     if k > j + 1 and nums[k] == nums[k - 1]:
                         continue
+
                     if nums[i] + nums[j] + nums[k] == 0:
-                        result.append([nums[i], nums[j], nums[k]])
-        return result
+                        results.append([nums[i], nums[j], nums[k]])
+
+        return results
+
+a = Solution()
+nums = [-1,0,1,2,-1,-4]
+print(a.threeSum(nums))
