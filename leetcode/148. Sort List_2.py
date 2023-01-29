37ecdc68e60be512cb8b074294ab49fd4813499a
sungslee <k8440009@gmail.com>
2023-01-30 04:24:08 +0900

    add 148

---
+++ leetcode/148. Sort List_2.py
@@ -0,0 +1,31 @@
+"""
+    리스트 정렬
+    내장 함수로 구현
+"""
+from typing import Optional, List
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # 연결 리스트 -> 파이썬 리스트
+        p = head
+        lst: List = []
+        while p:
+            lst.append(p.val)
+            p = p.next
+        # 정렬
+        lst.sort()
+
+        # 파이썬 리스트 -> 연결 리스트
+        p = head
+        for i in range(len(lst)):
+            p.val = lst[i]
+            p = p.next
+        return head
