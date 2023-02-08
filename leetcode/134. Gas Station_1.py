d85662a5f7f5f814194727a6b6a2590796aae41e
sungslee <k8440009@gmail.com>
2023-02-09 08:33:51 +0900

    add 134

---
+++ leetcode/134. Gas Station_1.py
@@ -0,0 +1,27 @@
+"""
+    주유소
+    https://leetcode.com/problems/gas-station/
+
+    브루트 포스
+    - 타임아웃
+"""
+from typing import List
+
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        for start in range(len(gas)):
+            fuel = 0
+            for i in range(start, len(gas) + start):
+                index = i % len(gas)
+
+                can_travel = True
+                if gas[index] + fuel < cost[index]:
+                    can_travel = False
+                    break
+                else:
+                    fuel += gas[index] - cost[index]
+
+            if can_travel:
+                return start
+        return -1
