102a20a5565ba19455a037b4123b70a09c161756
sungslee <k8440009@gmail.com>
2023-02-18 17:37:41 +0900

    review 121

---
+++ leetcode/121. Best Time to Buy and Sell Stock_0_1_0.py
@@ -0,0 +1,17 @@
+"""
+    주식을 사고팔기 가장 좋은 시점
+    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+    1. 브루트 포스
+    : timeout
+"""
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_price = 0
+        for i, price in enumerate(prices):
+            for j in range(i, len(prices)):
+                max_price = max(prices[j] - price, max_price)
+        return max_price
