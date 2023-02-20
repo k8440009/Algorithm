0b97527688f3f70362e142d0f0b2ab27af7fdf97
sungslee <k8440009@gmail.com>
2023-02-20 22:37:06 +0900

    review 200

--- leetcode/200. Number of Islands_0_2_1.py
+++ leetcode/200. Number of Islands_0_2_1.py
@@ -1,5 +1,6 @@
 """
     섬의 개수
+    https://leetcode.com/problems/number-of-islands/
     dfs
 """
 from typing import List
@@ -7,25 +8,27 @@ from typing import List
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(i, j):
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) \
-                    or grid[i][j] != '1':
+        def dfs(r, c):
+            if r < 0 or r >= len(grid) or \
+                    c < 0 or c >= len(grid[0]) or \
+                    grid[r][c] != "1":
                 return
 
-            grid[i][j] = 0
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-            dfs(i, j + 1)
-            dfs(i, j - 1)
+            grid[r][c] = "0"
 
-        count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    dfs(i, j)
-                    count += 1
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
 
-        return count
+        cnt = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == "1":
+                    dfs(r, c)
+                    cnt += 1
+
+        return cnt
 
 
 a = Solution()
