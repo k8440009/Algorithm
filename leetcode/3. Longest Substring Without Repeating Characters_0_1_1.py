72f431becb5384f311ee4c9d7d56c90fcd71876c
sungslee <k8440009@gmail.com>
2023-03-17 07:10:56 +0900

    reveiw 3

--- leetcode/3. Longest Substring Without Repeating Characters_0_1_1.py
+++ leetcode/3. Longest Substring Without Repeating Characters_0_1_1.py
@@ -1,30 +1,30 @@
 """
-    Given a string s, find the length of the longest
-    substring without repeating characters.
+    중복 문자 없는 가장 긴 부분 문자열
+    https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
-    ## 나의 풀이방법
-    - 재귀, 스택
+    슬라이딩 윈도우와 투 포인터
 """
+
+
 class Solution:
-    def longest_substring(self, substr: str) -> str:
-        for char in sorted(set(substr)):
-            suffix = substr[substr.index(char):]
-
-            print(suffix)
-            # if set(substr) == set(suffix):
-                # return char + self.longest_substring(suffix)
-
-        return ''
-
     def lengthOfLongestSubstring(self, s: str) -> int:
-        data = self.longest_substring(s)
+        used = {}
+        max_length = start = 0
 
-        return len(data)
+        for index, char in enumerate(s):
+            """
+                1. 이미 등장했던 문자라면 'start' 위치 갱신
+                2. 무작정 start를 변경하면 안되고, 
+                슬라이딩 윈도우의 바깥에 있는 문자만 옮긴다.
+            """
+            if char in used and start <= used[char]:
+                # print("start={0}, used[char]={1}".format(start, used))
+                start = used[char] + 1
+            else:  # 최대 부분 문자열 길이 갱신
+                # print("index={0}, start={1}".format(index, start))
+                max_length = max(max_length, index - start + 1)
 
-a = Solution()
-s = "abcabcbb"
-print(a.lengthOfLongestSubstring(s))
-# s = "bbbbb"
-# print(a.lengthOfLongestSubstring(s))
-# s = "pwwkew"
-# print(a.lengthOfLongestSubstring(s))
+            # 현재 문자의 위치 삽입
+            used[char] = index
+
+        return max_length
